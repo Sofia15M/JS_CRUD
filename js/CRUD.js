b38209c005db76e6009d5cf6e425a23fbb8686d2
Sofia15M <sofiaortizacevedo2007@gmail.com>
2023-10-06 14:56:01 -0500

    Metodo Create

--- js/CRUD.js
+++ js/CRUD.js
@@ -1,1 +1,33 @@
-export class CRUD{}
+export class CRUD{
+	#crudsofi = null;
+    #data = null;
+  
+	constructor(crudsofi){
+ 		this.#setcrudsofi(crudsofi);
+      	this.#setData();
+  	}
+
+	#setcrudsofi(crudsofi){
+		this.#crudsofiValidate(crudsofi);
+        this.#crudsofi = crudsofi;
+	}
+    
+    #setData(){
+    	this.#data = [];
+	}
+    
+    #crudsofiValidate(crudsofi){
+		if(crudsofi == undefined) throw new Error("Table crudsofi required");
+	}
+    
+    create(data){
+        this.#data.push(data);
+        return this.#data.length;
+    }
+
+	read(id){}
+
+	update(id, data){}
+
+	delete(id){}
+}
